1567ccf88f8192c3f6f43fcdf3a8efb8d26f00fd
Hrishikesh Rana <itsranahrishikesh@gmail.com>
2026-06-30 01:03:37 +0530

    fix: fixed typo in database  schema

---
+++ packages/database/prisma/migrations/20260629193303_fixed_typo/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `{}` on the `candidate` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "candidate" DROP COLUMN "{}",
+ADD COLUMN     "metadata" JSONB NOT NULL DEFAULT '{}';
